51d5d734f07437a088a3dfcd405af1dbfd1b1b55
tbduong <tiffaniduong@gmail.com>
2016-07-09 14:25:59 -0700

    create new div for P2 and testing columns

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).on("ready", function(){
+
+}
+
+
+// var playerOne = [".image", "null", "null", "null"];
+//
+// function move(){
+//     playerOne.pop(null);
+//     playerOne.unshift(".image");
+//     if(playerOne === (playerOne.length-1)){
+//       console.log("test");
+//     }
+//
+// }
+
+//
+// $("#startAnimation").click(function(){
+//     $(".toBeAnimated").animate({
+//         marginLeft: "+=250px",
+//     }, 1000 );
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
