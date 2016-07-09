2b4385a00620af1da4f8262f5e1e47f09d490762
tbduong <tiffaniduong@gmail.com>
2016-07-09 16:05:32 -0700

    create ability to move player 1 on keypress

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
+//have playerOne animate towards target column(finish line)
+//set variables and position of players
+//create animate function that moves player to target
+  //have it trigger on KEYPRESS, key #
+
 $(document).on("ready", function(){
 
-}
+$(document).on("keypress", function movePlayer(event) {
+  if (event.which == (113, 87)); //q key
+    $('.playerOne').animate({left: '+=100'});
+  });
+
+  // if ('.playerOne' === '.col-md-1'){
+  //   alert("winner!");
+  //
+  // });
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // var playerOne = [".image", "null", "null", "null"];
@@ -20,29 +55,3 @@ $(document).on("ready", function(){
 //         marginLeft: "+=250px",
 //     }, 1000 );
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
