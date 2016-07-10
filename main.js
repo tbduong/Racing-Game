5f8bdc9e468d2917dd4849f083bbd1bcf5a50d72
tbduong <tiffaniduong@gmail.com>
2016-07-10 01:33:18 -0700

    tweaked syntax,  syntax, begin styling, added players

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 //set variables and position of players
 //create animate function that moves player to target
   //have it trigger on KEYPRESS, key #
-
 $(document).on("ready", function(){
 $('.jumbotron').hide();
 var playerOneCount = 0;
+var playerTwoCount = 0;
+// var playerThreeCount = 0;
+// var playerFourCount = 0;
 var winCount = 0;
   console.log ("sanity check");
 
@@ -20,7 +22,6 @@ var winCount = 0;
 $(document).on("keypress", function movePlayer1(event) {
   if ((playerOneCount>=60) || (winCount===1)){
     console.log("Player 1 is the Winner!");
-    //stops player from going on.
   } else if (event.which == (113)){ //q key
     $('.playerOne').stop().animate({left: '+=50', speed: 'fast'});
     playerOneCount ++;
@@ -30,86 +31,49 @@ $(document).on("keypress", function movePlayer1(event) {
   }
 }
 }
+});
 $(document).on("keypress", function movePlayer2(event){
   if ((playerTwoCount>=60) || (winCount===1)){
-    console.log("Player 3 is the Winner!");     //stops player from going on.
+    console.log("Player 3 is the Winner!");
   } else if (event.which == (112)){ //p key
       $('.playerTwo').stop().animate({left: '+=50', speed: 'fast'});
       playerTwoCount ++;
       console.log(playerTwoCount);
-
-    if(playerTowCount === 65){
+    if(playerTwoCount === 65){
     alert("PLAYER 2 wins!");
-    }
-}
-});
-$(document).on("keypress", function movePlayer3(event) {
-  if ((playerThreeCount>=60) || (winCount===1)){
-    console.log("Player 3 is the Winner!");
-    //stops player from going on.
-  } else if (event.which == (122)){ //z key
-  $('.playerThree').stop().animate({left: '+=50', speed: 'fast'});
-  playerFourCount ++;
-  console.log(playerThreeCount);
-  if(playerThreeCount === 65){
-    alert("PLAYER 3 wins!");
-  }
-}
-});
-$(document).on("keypress", function movePlayer4(event) {
-  if ((playerFourCount>=60) || (winCount===1)){
-    console.log("Player 4 is the Winner!");
-    //stops player from going on.
-  } else if (event.which == (109)){ //m key
-  $('.playerFour').stop().animate({left: '+=50', speed: 'fast'});
-  playerFourCount ++;
-  console.log(playerFourCount);
-  if(playerFourCount === 65){
-    alert("PLAYER 4 wins!");
   }
 }
 
 });
+
 //Reset Button
     $('.btn').on("click", function (reset){
       location.reload();
     });
+
 });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var playerOne = [".image", "null", "null", "null"];
-//
-// function move(){
-//     playerOne.pop(null);
-//     playerOne.unshift(".image");
-//     if(playerOne === (playerOne.length-1)){
-//       console.log("test");
-//     }
-//
+// $(document).on("keypress", function movePlayer3(event) {
+//   if ((playerThreeCount>=60) || (winCount===1)){
+//     console.log("Player 3 is the Winner!");
+//     //stops player from going on.
+//   } else if (event.which == (122)){ //z key
+//   $('.playerThree').stop().animate({left: '+=50', speed: 'fast'});
+//   playerFourCount ++;
+//   console.log(playerThreeCount);
+//   if(playerThreeCount === 65){
+//     alert("PLAYER 3 wins!");
+//   }
 // }
-
 //
-// $("#startAnimation").click(function(){
-//     $(".toBeAnimated").animate({
-//         marginLeft: "+=250px",
-//     }, 1000 );
-// });
+// $(document).on("keypress", function movePlayer4(event) {
+//   if ((playerFourCount>=60) || (winCount===1)){
+//     console.log("Player 4 is the Winner!");
+//     //stops player from going on.
+//   } else if (event.which == (109)){ //m key
+//   $('.playerFour').stop().animate({left: '+=50', speed: 'fast'});
+//   playerFourCount ++;
+//   console.log(playerFourCount);
+//   if(playerFourCount === 65){
+//     alert("PLAYER 4 wins!");
+//   }
+// }
