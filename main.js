75d620d28658c01293135a0fb57f6bce2491aebb
tbduong <tiffaniduong@gmail.com>
2016-07-10 00:17:38 -0700

    mess with margins and divs.. try to target margin as finish line, background image add

--- main.js
+++ main.js
@@ -4,34 +4,36 @@
   //have it trigger on KEYPRESS, key #
 
 $(document).on("ready", function(){
-
-var counter = 0;
-
-//keypress function
-$(document).on("keypress", function movePlayer(event) {
-  if (counter=100){
-
-
-  }else if (event.which == (113)){ //q key
-  $('.playerOne').animate({left: '+=20', speed: 'fast'});{
-
-    }
-  }
-
-  {if (event.which == (112)){ //p key
-  $('.playerTwo').animate({left: '+=20', speed: 'fast'});
-  }
-
-
-
-
-
-}
-
-});
+$('.jumbotron').hide();
+var playerOneCount = 0;
+var winCount = 0;
+  console.log ("sanity check");
 
 //win conditions
+// function win (winner) {
+//      $( '.jumbotron' ).show( 15 );
+//      $('.winning').text(winner + "is the winner!");
+//      winCount++;
+// }
 
+//keypress function
+$(document).on("keypress", function movePlayer1(event) {
+  if ((playerOneCount>=60) || (winCount===1)){
+    console.log("Player One is the Winner!");
+    //stops player from going on.
+  } else if (event.which == (113)){ //q key
+  $('.playerOne').stop().animate({left: '+=50', speed: 'fast'});
+  playerOneCount ++;
+  console.log(playerOneCount);
+  if(playerOneCount === 65){
+    alert("PLAYER 1 wins!");
+  }
+}
+//
+//   } if (event.which == (112)){ //p key
+//   $('.playerTwo').animate({left: '+=30', speed: 'fast'});
+// }
+//
 
 
 
@@ -39,6 +41,15 @@ $(document).on("keypress", function movePlayer(event) {
 
 
 });
+//Reset Button
+$('.btn').on("click", function (reset){
+  location.reload();
+});
+
+});
+
+
+
 
 
 
