1baa7e41fa669d070ab38cdfe28ec80cb3d5414c
tbduong <tiffaniduong@gmail.com>
2016-07-10 00:44:25 -0700

    add counter to all players-- only P1 working with keypress

--- main.js
+++ main.js
@@ -19,33 +19,62 @@ var winCount = 0;
 //keypress function
 $(document).on("keypress", function movePlayer1(event) {
   if ((playerOneCount>=60) || (winCount===1)){
-    console.log("Player One is the Winner!");
+    console.log("Player 1 is the Winner!");
     //stops player from going on.
   } else if (event.which == (113)){ //q key
-  $('.playerOne').stop().animate({left: '+=50', speed: 'fast'});
-  playerOneCount ++;
-  console.log(playerOneCount);
+    $('.playerOne').stop().animate({left: '+=50', speed: 'fast'});
+    playerOneCount ++;
+    console.log(playerOneCount);{
   if(playerOneCount === 65){
     alert("PLAYER 1 wins!");
   }
 }
-//
-//   } if (event.which == (112)){ //p key
-//   $('.playerTwo').animate({left: '+=30', speed: 'fast'});
-// }
-//
+}
+$(document).on("keypress", function movePlayer2(event){
+  if ((playerTwoCount>=60) || (winCount===1)){
+    console.log("Player 3 is the Winner!");     //stops player from going on.
+  } else if (event.which == (112)){ //p key
+      $('.playerTwo').stop().animate({left: '+=50', speed: 'fast'});
+      playerTwoCount ++;
+      console.log(playerTwoCount);
 
-
-
-
-
+    if(playerTowCount === 65){
+    alert("PLAYER 2 wins!");
+    }
+}
+});
+$(document).on("keypress", function movePlayer3(event) {
+  if ((playerThreeCount>=60) || (winCount===1)){
+    console.log("Player 3 is the Winner!");
+    //stops player from going on.
+  } else if (event.which == (122)){ //z key
+  $('.playerThree').stop().animate({left: '+=50', speed: 'fast'});
+  playerFourCount ++;
+  console.log(playerThreeCount);
+  if(playerThreeCount === 65){
+    alert("PLAYER 3 wins!");
+  }
+}
+});
+$(document).on("keypress", function movePlayer4(event) {
+  if ((playerFourCount>=60) || (winCount===1)){
+    console.log("Player 4 is the Winner!");
+    //stops player from going on.
+  } else if (event.which == (109)){ //m key
+  $('.playerFour').stop().animate({left: '+=50', speed: 'fast'});
+  playerFourCount ++;
+  console.log(playerFourCount);
+  if(playerFourCount === 65){
+    alert("PLAYER 4 wins!");
+  }
+}
 
 });
 //Reset Button
-$('.btn').on("click", function (reset){
-  location.reload();
+    $('.btn').on("click", function (reset){
+      location.reload();
+    });
 });
-
 });
 
 
