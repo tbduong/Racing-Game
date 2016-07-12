e0d266f8d219e67dc600a6ebacb7c96c5625cd12
tbduong <tiffaniduong@gmail.com>
2016-07-11 23:59:01 -0700

    reversed changes, to have scoreboard more functional

--- main.js
+++ main.js
@@ -1,79 +1,49 @@
-//have playerOne animate towards target column(finish line)
-//set variables and position of players
-//create animate function that moves player to target
-//have it trigger on KEYPRESS, key #
+
 $(document).on("ready", function(){
-  // console.log("sanity test!");
-  // var playerOneCount = 0;
-  // var playerTwoCount = 0;
-  // var winCount= 0;
+//variables needed to tally scoreboard-- still buggy
+  var playerOneCount = 0;
+  var playerTwoCount = 0;
+  var winCount= 0;
 
   var MOVE_AMT = 32;
   var CHAR_ONE = 113;
   var CHAR_TWO = 112;
-  var winCount = 0;
-  var playerOneCount=0;
-  var playerTwoCount=0;
+  //function to move:
 
-  var playerOne = {
-    charChode: CHAR_ONE,
-    num: 0,
-    position: 0,
-    $el: $(".player").eq(0),
-    name: "Seahorse 1"
-  };
-  var playerTwo = {
-    charChode: CHAR_TWO,
-    num: 1,
-    position: 0,
-    $el: $(".player").eq(1),
-    name: "Seahorse 2"
-  };
-  var players = [playerOne, playerTwo];
 
   //keypress function
-  $(document).on("keypress", function move(player) {
-    if ((playerOneCount===MOVE_AMT) || (winCount===1)){
+  $(document).on("keypress", function movePlayer1(event) {
+    if ((playerOneCount>=MOVE_AMT) || (winCount===1)){
     } else if (event.which == (CHAR_ONE)){ //q key
       $('.playerOne').css({"left": '+=35'});
-      player.position ++;
-
+      playerOneCount ++;
+    }   if(playerOneCount === MOVE_AMT){
+      $("#result").text("PLAYER 1 wins!");
+      winCount ++;
+      $("#playerOneScore").text(winCount);
     }
   });
+
   $(document).on("keypress", function movePlayer2(event){
-    if ((playerTwoCount===MOVE_AMT) || (winCount===1)){
+    if ((playerTwoCount>=MOVE_AMT) || (winCount===1)){
     } else if (event.which == (CHAR_TWO)){ //p key
       $('.playerTwo').css({"left": '+=35'});
-      player ++;
+      playerTwoCount ++;
 
-      if (function winCheck(num){
-        if(num === 1 ) {
-          if((playerOneCount === MOVE_AMT) || (winCount===1)) {
-            $("#result").text("PLAYER 1 wins!");
-            winCount ++;
-            $("#playerOneScore").text(winCount);//Win counter on scoreboard
-          }
-        } else if (num === 2) {
-          if((playerTwoCount === MOVE_AMT) || (winCount===1)){
-            $("#result").text("PLAYER 2 wins!");
-            winCount ++;
-            $("#playerTwoScore").text(winCount); //Win counter on scoreboard
-          }
-        }
-      });
+    }   if(playerTwoCount === MOVE_AMT){
+      $("#result").text("PLAYER 2 wins!");
+      winCount ++;
+      $("#playerTwoScore").text(winCount);
     }
-
-
   });
 
-
-  //Reset Button
+  //Reset Button-- remove/add classes, text, etc.
   function reset(){
     $(".btn").on("click", function handleClick(event){
-      $(".playerOne").clearQueue();
-      $(".playerOne").stop();
       $(".playerOne").css("left",("0"));
       $(".playerTwo").css("left",("0"));
+      $(".playerOne").removeClass("#result");
+
 
 
 
