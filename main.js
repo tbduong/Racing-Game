ed50bcaf732060468db51b0cea6d5363a1cb5fe4
tbduong <tiffaniduong@gmail.com>
2016-07-11 23:26:23 -0700

    working on refactoring move function.. divs fly off page now. need to debug

--- main.js
+++ main.js
@@ -3,80 +3,64 @@
 //create animate function that moves player to target
 //have it trigger on KEYPRESS, key #
 $(document).on("ready", function(){
-  console.log("sanity test!");
-  var playerOneCount = 0;
-  var playerTwoCount = 0;
-  var winCount= 0;
+  // console.log("sanity test!");
+  // var playerOneCount = 0;
+  // var playerTwoCount = 0;
+  // var winCount= 0;
 
   var MOVE_AMT = 32;
   var CHAR_ONE = 113;
   var CHAR_TWO = 112;
+  var winCount = 0;
+  var playerOneCount=0;
+  var playerTwoCount=0;
 
   var playerOne = {
-      charChode: CHAR_ONE,
-      number: 1,
-      position: 0,
-      $el: $(".player").eq(0),
-      name: "Seahorse 1"
+    charChode: CHAR_ONE,
+    num: 0,
+    position: 0,
+    $el: $(".player").eq(0),
+    name: "Seahorse 1"
   };
   var playerTwo = {
-      charChode: CHAR_TWO,
-      number: 2,
-      position: 0,
-      $el: $(".player").eq(1),
-      name: "Seahorse 2"
+    charChode: CHAR_TWO,
+    num: 1,
+    position: 0,
+    $el: $(".player").eq(1),
+    name: "Seahorse 2"
   };
-
   var players = [playerOne, playerTwo];
 
-
-
-
   //keypress function
-  $(document).on("keypress", function movePlayer1(event) {
-    if ((playerOneCount>=MOVE_AMT) || (winCount===1)){
+  $(document).on("keypress", function move(player) {
+    if ((playerOneCount===MOVE_AMT) || (winCount===1)){
     } else if (event.which == (CHAR_ONE)){ //q key
       $('.playerOne').css({"left": '+=35'});
-      playerOneCount ++;
-      //console.log(playerOneCount);
+      player.position ++;
 
-    }   if(playerOneCount === MOVE_AMT){
-      $("#result").text("PLAYER 1 wins!");
-      winCount ++;
-      $("#playerOneScore").text(winCount);
     }
-});
-
+  });
   $(document).on("keypress", function movePlayer2(event){
-    if ((playerTwoCount>=MOVE_AMT) || (winCount===1)){
+    if ((playerTwoCount===MOVE_AMT) || (winCount===1)){
     } else if (event.which == (CHAR_TWO)){ //p key
       $('.playerTwo').css({"left": '+=35'});
-      playerTwoCount ++;
-      //console.log(playerTwoCount);
+      player ++;
 
-    }   if(playerTwoCount === MOVE_AMT){
-      $("#result").text("PLAYER 2 wins!");
-      winCount ++;
-      $("#playerTwoScore").text(winCount);
-
-      //TODO: atm the scoreboard continues to add to playerOneScore from both players' wins. Player 2 wins accumulate inside there as well...
-      //need to make two separate variables winOne and winTwo, so append into player Two's box
-
-      // if (function winCheck(num){
-      //   if(num === 1 ) {
-      //     if(playerOneCount === 32) {
-      //       $("#result").text("PLAYER 1 wins!");
-      //       winCount ++;
-      //       $("#playerOneScore").text(winCount);//Win counter on scoreboard
-      //     }
-      //   } else if (num === 2) {
-      //     if(playerTwoCount === 32) {
-      //       $("#result").text("PLAYER 2 wins!");
-      //       winCount ++;
-      //       $("#playerTwoScore").text(winCount); //Win counter on scoreboard
-      //     }
-      //   }
-      // });
+      if (function winCheck(num){
+        if(num === 1 ) {
+          if((playerOneCount === MOVE_AMT) || (winCount===1)) {
+            $("#result").text("PLAYER 1 wins!");
+            winCount ++;
+            $("#playerOneScore").text(winCount);//Win counter on scoreboard
+          }
+        } else if (num === 2) {
+          if((playerTwoCount === MOVE_AMT) || (winCount===1)){
+            $("#result").text("PLAYER 2 wins!");
+            winCount ++;
+            $("#playerTwoScore").text(winCount); //Win counter on scoreboard
+          }
+        }
+      });
     }
 
 
